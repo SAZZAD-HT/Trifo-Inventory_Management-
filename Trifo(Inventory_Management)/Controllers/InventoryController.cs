72782552cb9ae3294977ebeb23151931478783bb
SAZZAD HOSSAIN TASNIM <sazzadaiub1@gmail.com>
2023-03-17 22:36:34 +0600

    s

--- Trifo(Inventory_Management)/Controllers/InventoryController.cs
+++ Trifo(Inventory_Management)/Controllers/InventoryController.cs
@@ -51,28 +51,28 @@ namespace Trifo_Inventory_Management_.Controllers
         }
         public ActionResult Line_chart_Monthly_sale()
         {
-            var db = new dbContext(); // Replace with your own DbContext class
-            var monthlySales = db.Products
-                .Where(s => s.sell_date.ToString().Month== DateTime.Now.Month && s.sell_date.Year == DateTime.Now.Year)
-                .GroupBy(s => s.sell_date.Day)
-                .Select(g => new { Day = g.Key, TotalSales = g.Sum(s => s.Quantity * s.Price) })
-                .OrderBy(g => g.Day)
-                .ToList();
+            //var db = new dbContext(); // Replace with your own DbContext class
+            //var monthlySales = db.Products
+            //    .Where(s => s.sell_date.ToString().Month== DateTime.Now.Month && s.sell_date.Year == DateTime.Now.Year)
+            //    .GroupBy(s => s.sell_date.Day)
+            //    .Select(g => new { Day = g.Key, TotalSales = g.Sum(s => s.Quantity * s.Price) })
+            //    .OrderBy(g => g.Day)
+            //    .ToList();
 
-            // Create a chart to display the monthly sales data
-            var chart = new Chart(width: 600, height: 400)
-                .AddTitle("Monthly Sales Chart")
-                .AddSeries(
-                    chartType: "column",
-                    xValue: monthlySales.Select(s => s.Day).ToArray(),
-                    yValues: monthlySales.Select(s => s.TotalSales).ToArray()
-                );
+            //// Create a chart to display the monthly sales data
+            //var chart = new Chart(width: 600, height: 400)
+            //    .AddTitle("Monthly Sales Chart")
+            //    .AddSeries(
+            //        chartType: "column",
+            //        xValue: monthlySales.Select(s => s.Day).ToArray(),
+            //        yValues: monthlySales.Select(s => s.TotalSales).ToArray()
+            //    );
 
-            // Convert the chart to a byte array for display in the view
-            var chartData = chart.GetBytes();
+            //// Convert the chart to a byte array for display in the view
+            //var chartData = chart.GetBytes();
 
-            // Pass the chart data to the view
-            ViewBag.ChartData = chartData;
+            //// Pass the chart data to the view
+           // ViewBag.ChartData = chartData;
             return View();
         }
         public ActionResult Pie_chart_Monthly_sale_Category()
