52821d6789e062295b5539f107b56425180a0c18
SAZZAD HOSSAIN TASNIM <sazzadaiub1@gmail.com>
2023-03-29 20:57:49 +0600

    Add The Client Gives NEw requirement and Ui design The project is Shifting its Outlook

---
+++ Trifo(Inventory_Management)/Controllers/MemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Trifo_Inventory_Management_.Controllers
+{
+    public class MemoController : Controller
+    {
+        // GET: Memo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
