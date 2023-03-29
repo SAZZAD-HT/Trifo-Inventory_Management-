b11b6730f6ed11f40e12836b1308c667aaaa9626
SAZZAD HOSSAIN TASNIM <sazzadaiub1@gmail.com>
2023-03-29 22:56:01 +0600

    Implementing SOLID

---
+++ Data AcessLayer/Models/ef/ImportList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Trifo_Inventory_Management_.Models.ef
+{
+    public class ImportList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+
+        public int Import { get; set; }
+
+        public string sProduct_name { get; set; }
+
+        public string country { get; set; }
+
+
+
+
+        public String sDescription { get; set; }
+
+
+        
+       
+        public string d_Quantity { get; set; }
+        public string cust_quantity { get; set; }
+        public String Customer_name { get; set; }
+        public string CustomerEmail { get; set; }
+        public string Customer_mobiler { get; set; }
+    }
+}
